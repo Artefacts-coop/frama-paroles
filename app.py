57cb9b6b33d4e410f460e240d14549f697b31bab
lluc <leger.luc@gmail.com>
2024-06-07 17:48:17 +0200

    [datavis] Top des contributeurs

--- app.py
+++ app.py
@@ -3,10 +3,13 @@ import pandas as pd
 import streamlit as st
 from streamlit_timeline import timeline
 import altair as alt
+import plotly.express as px
 import datetime
 
 
 def main():
+
+    NB_TOP = 20 # Nombre de contributeurs les plus actifs a afficher
 
     db_channels = []
 
@@ -70,6 +73,48 @@ def main():
     c.bar_chart(chart_data, x="genres", y="nombre de posts", color="col3")
 
     # --------------------------------------------
+
+    # Requête pour les 20 membres qui ont créé le plus de posts
+    query = """
+    SELECT members.username, COUNT(posts.id) AS nb_posts, members.gender
+    FROM posts
+    LEFT JOIN members ON posts.member_id = members.id
+    WHERE channel_id IN (SELECT id FROM channels WHERE name IN (%s))
+    AND posts.create_at >= %s AND posts.create_at < %s
+    GROUP BY posts.member_id
+    ORDER BY nb_posts ASC
+    LIMIT %s
+    """ % (
+        ",".join(['"' + channel + '"' for channel in channels]),
+        datetime.datetime(year[0], 1, 1).timestamp(),
+        datetime.datetime(year[1], 12, 31).timestamp(),
+        NB_TOP,
+    )
+    results = requete(conn, query)
+    top_members = results
+
+    # Données d'exemple
+    data = pd.DataFrame(
+        {
+            "Contributeurs": [member[0] for member in top_members],
+            "Nb posts": [member[1] for member in top_members],
+            "genre": ["Homme" if member[2] == "h" else "Femme" for member in top_members],
+        }
+    )
+
+    # Créer le graphique à barres horizontales avec étiquettes
+    fig = px.bar(
+        data,
+        x="Nb posts",
+        y="Contributeurs",
+        color="genre",
+        orientation="h",
+        text="Nb posts",
+        barmode="group",
+        color_discrete_sequence=["#FFC0CB", "#AAC7E6"],
+        title=f"Top {NB_TOP} des contributeurs",
+        )
+    c.plotly_chart(fig)
 
     # Fermeture de la connexion à la base de données
     conn.close()
